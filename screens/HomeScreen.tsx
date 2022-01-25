be3f621b57397a6546ac574061b918a0c4ef3962
Megan Penn <penn.meg@gmail.com>
2022-01-24 20:01:36 -0500

    Repositioning of some items on the homescreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,6 @@
-import {
-  StyleSheet,
-  TouchableOpacity,
-  Dimensions,
-  TextInput,
-  ScrollView,
-} from "react-native";
+import { StyleSheet, TouchableOpacity, Dimensions, View } from "react-native";
 import { useState } from "react";
-import { Text, View } from "../components/Themed";
+import { Text } from "../components/Themed";
 import Instructions from "./HomeScreen/Instructions";
 import MeetingDetails from "./HomeScreen/MeetingDetails";
 
@@ -21,43 +15,56 @@ export default function HomeScreen({ navigation }) {
       <View style={styles.header}>
         <Text style={styles.title}>This Meeting Could Have Been An Email</Text>
       </View>
-      <TouchableOpacity
-        style={[styles.tab, { backgroundColor: "#c0aeff" }]}
-        onPress={() => setShowContent("Instructions")}
-      >
-        <View style={[styles.inner, { backgroundColor: "#d6c9ff" }]}>
-          <Text>Welcome</Text>
+      <View style={{ width: "100%" }}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => setShowContent("Instructions")}
+          style={[
+            {
+              backgroundColor: "#c0aeff",
+            },
+            styles.tab,
+          ]}
+        >
+          <View style={[styles.inner, { backgroundColor: "#d6c9ff" }]}>
+            <Text style={{ fontWeight: "bold" }}>Welcome</Text>
+          </View>
+        </TouchableOpacity>
+        <View
+          style={[
+            styles.content,
+            {
+              backgroundColor: "#d6c9ff",
+              height: showContent === "Instructions" ? screenHeight - 190 : 0,
+              // padding: showContent === "Instructions" ? 20 : 0,
+            },
+          ]}
+        >
+          <Instructions />
         </View>
-      </TouchableOpacity>
-      <View
-        style={[
-          styles.content,
-          {
-            backgroundColor: "#d6c9ff",
-            height: showContent === "Instructions" ? screenHeight - 190 : 0,
-            padding: showContent === "Instructions" ? 20 : 0,
-          },
-        ]}
-      >
-        <Instructions />
       </View>
-      <TouchableOpacity
-        style={[styles.tab, { backgroundColor: "#d6c9ff" }]}
-        onPress={() => setShowContent("Details")}
-      >
-        <View style={[styles.inner, { backgroundColor: "white" }]}>
-          <Text>Meeting Details</Text>
+      <View style={{ width: "100%", alignSelf: "baseline" }}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={[styles.tab, { backgroundColor: "#d6c9ff" }]}
+          onPress={() => setShowContent("Details")}
+        >
+          <View style={[styles.inner, { backgroundColor: "white" }]}>
+            <Text style={{ fontWeight: "bold" }}>Meeting Details</Text>
+          </View>
+        </TouchableOpacity>
+        <View
+          style={[
+            styles.content,
+
+            {
+              height: showContent === "Details" ? screenHeight - 190 : 0,
+              backgroundColor: "white",
+            },
+          ]}
+        >
+          <MeetingDetails navigation={navigation} />
         </View>
-      </TouchableOpacity>
-      <View
-        style={[
-          styles.content,
-          {
-            height: showContent === "Details" ? screenHeight - 190 : 0,
-          },
-        ]}
-      >
-        <MeetingDetails navigation={navigation} />
       </View>
     </View>
   );
@@ -76,7 +83,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#c0aeff",
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: "bold",
     textAlign: "center",
   },
@@ -87,11 +94,14 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 32,
     borderTopRightRadius: 32,
     padding: 10,
-    alignItems: "center",
-    // shadow
+    paddingLeft: 20,
+    shadowColor: "#18191A",
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
+    width: "100%",
   },
   content: {
     width: "100%",
-    padding: 20,
+    paddingHorizontal: 20,
   },
 });
