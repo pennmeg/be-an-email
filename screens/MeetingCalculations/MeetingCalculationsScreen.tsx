7cd0f00c443cb5bd77850ab64bb6c8a7dadd5bd3
Megan Penn <penn.meg@gmail.com>
2022-01-31 22:34:37 -0500

    Add image and styles to calculations screen

--- screens/MeetingCalculations/MeetingCalculationsScreen.tsx
+++ screens/MeetingCalculations/MeetingCalculationsScreen.tsx
@@ -1,15 +1,14 @@
 import {
   StyleSheet,
   TouchableOpacity,
-  Dimensions,
-  TextInput,
-  ScrollView,
+  Text as RNText,
+  Image,
   ActivityIndicator,
+  Dimensions,
 } from "react-native";
 import { useState, useEffect, Fragment } from "react";
-import { Text, View } from "../../components/Themed";
-import Instructions from "../HomeScreen/Sections/Instructions";
-import MeetingDetails from "../HomeScreen/Sections/MeetingDetails/MeetingDetails";
+import { View } from "../../components/Themed";
+import UndrawImage from "../../assets/images/undraw_Celebration_re_kc9k.png";
 
 // Average, FT, salaried employee works 40 hours a week
 // Based on this, the average salaried person works 2,080 (40 x 52) hours a year
@@ -18,6 +17,8 @@ const HOURS_PER_YEAR = 2080;
 export default function MeetingCalculationsScreen({ navigation, route }) {
   const [isLoading, setIsLoading] = useState(true);
   const [meetingTotal, setMeetingTotal] = useState(null);
+
+  const screenWidth = Dimensions.get("screen").width;
 
   const formatDollar = Intl.NumberFormat("en-US");
 
@@ -58,13 +59,33 @@ export default function MeetingCalculationsScreen({ navigation, route }) {
         <ActivityIndicator size="large" color="#000000" />
       ) : (
         <Fragment>
-          <Text>Congratulations!</Text>
-          <Text>{`You meeting cost approximately $${meetingTotal}`}</Text>
+          <View style={styles.borderContainer}>
+            <RNText style={styles.title}>Congratulations!</RNText>
+            <View style={styles.purpleBorder}></View>
+          </View>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            <Image width={screenWidth} source={UndrawImage} />
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <RNText>Your meeting cost approximately </RNText>
+            <RNText style={{ fontWeight: "bold" }}>{`$${meetingTotal}`}</RNText>
+          </View>
           <TouchableOpacity
-            style={styles.button}
             onPress={() => navigation.navigate("Home")}
+            style={{
+              marginTop: 12,
+              padding: 12,
+              backgroundColor: "#A7A0FD",
+              borderColor: "#A7A0FD",
+              borderWidth: 1,
+              width: "90%",
+              alignItems: "center",
+              borderRadius: 8,
+            }}
           >
-            <Text style={{ color: "white" }}>Go Home</Text>
+            <RNText style={{ color: "black", fontWeight: "bold" }}>
+              Go Home
+            </RNText>
           </TouchableOpacity>
         </Fragment>
       )}
@@ -78,13 +99,31 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     paddingTop: 30,
-    backgroundColor: "#c0aeff",
+    backgroundColor: "white",
   },
-  button: {
-    backgroundColor: "black",
-    alignItems: "center",
-    padding: 8,
-    margin: 25,
-    width: "90%",
+  title: {
+    fontWeight: "bold",
+    fontSize: 32,
+    textAlign: "center",
+    lineHeight: 38,
+  },
+  lineHeight: {
+    lineHeight: 18,
+  },
+  borderContainer: {
+    position: "relative",
+    justifyContent: "flex-start",
+    paddingBottom: 8,
+    marginHorizontal: 12,
+    marginVertical: 12,
+  },
+  purpleBorder: {
+    height: 18,
+    width: 270,
+    backgroundColor: "#A7A0FD",
+    position: "absolute",
+    top: 19,
+    left: -2,
+    zIndex: -1,
   },
 });
