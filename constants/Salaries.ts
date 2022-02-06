71c65cdc70ec14d047470e459282375b0a2ec942
Megan Penn <penn.meg@gmail.com>
2022-02-05 20:14:59 -0500

    Add salaries arry

--- constants/Salaries.ts
+++ constants/Salaries.ts
@@ -3,3 +3,9 @@ export const formatDollar = Intl.NumberFormat("en-US");
 // Average, FT, salaried employee works 40 hours a week
 // Based on this, the average salaried person works 2,080 (40 x 52) hours a year
 export const HOURS_PER_YEAR = 2080;
+
+export const SALARIES = [
+  25000, 50000, 75000, 100000, 125000, 150000, 175000, 200000, 225000, 250000,
+  275000, 300000, 350000, 400000, 450000, 500000, 550000, 600000, 650000,
+  700000, 750000, 800000, 850000, 900000, 950000, 1000000,
+];
